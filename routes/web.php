80acbd288096db13c9c7df156011df5e106134fd
Ariel Araneda <ariel.arzu@outlook.com>
2024-04-18 16:38:18 -0400

    auth

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::group(
-    ['prefix' => 'admin/dashboard'], function(){
+    ['prefix' => 'admin/dashboard', 'middleware' => 'auth', 'admin'], function(){
         Route::resources([
             'product' => ProductController::class,
             'category' => CategoryController::class
